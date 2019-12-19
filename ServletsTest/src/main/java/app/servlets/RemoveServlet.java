207bae9ebbf5e15348ccc5d9f0ee12d96f7d681b
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-12-19 18:47:02 +0200

    change ServletTest

--- ServletsTest/src/main/java/app/servlets/RemoveServlet.java
+++ ServletsTest/src/main/java/app/servlets/RemoveServlet.java
@@ -5,11 +5,13 @@ import app.model.Model;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/remove")
 public class RemoveServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("views/remove.jsp");
