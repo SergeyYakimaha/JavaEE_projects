09856815f39740bbdf6d11199ee9b96dfe713af1
SergeyYakimaha <s.yakimaha78@gmail.com>
2020-01-11 14:53:02 +0200

    change ServletsTest

--- ServletsTest/src/main/java/app/servlets/ContentServlet.java
+++ ServletsTest/src/main/java/app/servlets/ContentServlet.java
@@ -9,19 +9,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+
+        printWriter.println("<html>");
+        printWriter.println("<head>");
+        printWriter.println("<title>Content page</title>");
+        printWriter.println("</head>");
+        printWriter.println("<body>");
+        printWriter.println("<h2 style=\"color: cornflowerblue\">Main content</h2>");
+        printWriter.println("</body>");
+        printWriter.println("</head>");
+        printWriter.println("</html>");
+
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/views/jsp/Content.jsp");
         requestDispatcher.forward(req, resp);
-        //        System.out.println(req.getSession().isNew());
-//
-//        if (session == null)
-//            System.out.println("SessionID = null");
-//        else
-//            System.out.println("SessionID = " + session.getId());
+
+        String s = req.getSession().getId();
     }
 }
