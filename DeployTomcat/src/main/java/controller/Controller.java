c6802c08965ca4a450ed1a8b31b732bfc282583d
SergeyYakimaha <s.yakimaha78@gmail.com>
2020-01-13 22:50:53 +0200

    commit

---
+++ DeployTomcat/src/main/java/controller/Controller.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class Controller extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("JSP/hello.jsp").forward(req, resp);
+    }
+}
